8fb5b6aef9ef58dceca90975d1e59c742d6f0c2d
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-02-25 16:57:00 +0100

    Exercice 2 Rails Wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
+require 'faker'
+
+# Générer 10 articles
+10.times do |i|
+  title = Faker::Book.title
+  content = Faker::Lorem.paragraph(sentence_count: 5)
+
+  puts "Article ##{i + 1}"
+  puts "Titre : #{title}"
+  puts "Contenu : #{content}"
+  puts '-' * 50
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Example:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
